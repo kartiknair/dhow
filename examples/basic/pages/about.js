45775612ca3c330fd56ad13909a9b8301f27fdff
fsoc <root@fsoc.space>
2021-07-30 19:53:08 +0200

    Add a basic example

---
+++ examples/basic/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => (<>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat ornare condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras sollicitudin.
+    </p>
+
+    <p>
+        Ex non dictum laoreet, mauris nisl finibus velit, vitae hendrerit turpis ante ac ligula. Suspendisse vitae porta mi. Phasellus sed eleifend tortor, vel mattis lorem. Nulla ipsum nibh, congue at velit a, mollis feugiat neque.
+    </p>
+</>)
+
+export default About
---
+++ examples/basic/pages/index.js
@@ -0,0 +1,19 @@
+const Home = () => (<>
+    <img src={'/dhow.jpg'} alt={'Dhow'} />
+
+    <p>
+        JSX-powered SSG for Node.js. Write logic like React with a directory-structure like Next.js but generate plain HTML with no client side JS.
+    </p>
+
+    <p>
+        <a href={'/about'}>Read more...</a>
+    </p>
+
+    <p>
+        <small>
+            The image above is licensed under the <a href={'https://creativecommons.org/licenses/by-sa/3.0/deed.en'}>Creative Commons Attribution-Share Alike 3.0 Unported</a> license and was taken from <a href={'https://en.wikipedia.org/wiki/File:Dhow01.JPG'}>Wikipedia</a>.
+        </small>
+    </p>
+</>)
+
+export default Home
